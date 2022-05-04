17115af49f08b0c38999e66de00b714ba62656a7
winvale <harledvalencia01@gmail.com>
2022-05-03 23:20:52 -0500

    mejoras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'freelancer-angular';
+  title = 'Portafolio-Edwin-Valencia';
   imagenes = [
     {
-      imagenSrc:'https://images.unsplash.com/photo-1497215728101-856f4ea42174?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imagenSrc:'assets/img/certificados/CertificadoJavaScri.PNG',
       imagenAlt:'1',
     },
     {
-      imagenSrc: 'https://images.unsplash.com/photo-1642670310920-6f4e3a3adee3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+      imagenSrc: 'assets/img/certificados/CertificacionEnResponsive.PNG',
       imagenAlt: '2',
     },
     {
-      imagenSrc: 'https://images.unsplash.com/photo-1642570517818-99c0fd6f0349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+      imagenSrc: 'assets/img/certificados/Certificateingles.PNG',
       imagenAlt: '3',
     },
     {
-      imagenSrc: 'https://images.unsplash.com/photo-1642649149963-0ef6779df6c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imagenSrc: 'assets/img/certificados/certificadoContablidad.PNG',
       imagenAlt: '4',
     },
     {
-      imagenSrc: 'https://images.unsplash.com/photo-1642618215095-3523a9a36893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+      imagenSrc: 'assets/img/certificados/Certificateprofuturo.PNG',
       imagenAlt: '5',
     },
   ]
